64f1f28c6f92fcb64aeb8ba2f109d02c166f4507
Vishnu <codemaker2015@gmail.com>
2024-09-09 09:20:33 +0530

    Initial commit

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //#region Bluetooth Low Energy
+
+    //public void StartScanLeDevices()
+    //{
+    //    if (Application.platform != RuntimePlatform.Android)
+    //        return;
+
+    //    BluetoothConnector.CallStatic("StartScanLeDevices");
+    //}
+
+    //public void StopScanLeDevices()
+    //{
+    //    if (Application.platform != RuntimePlatform.Android)
+    //        return;
+
+    //    BluetoothConnector.CallStatic("StopScanLeDevices");
+    //}
+
+    //public void LeScanStatus(string status)
+    //{
+    //    Debug.Log("Le Scan Status: " + status);
+    //}
+
+    //public void NewLeDeviceFound(string data)
+    //{
+    //    Debug.Log("New Le Device Found: " + data);
+    //}
+    //#endregion
+
+    
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity3D Android Bluetooth Demo 
+
+Simplest library for bluetooth interactions in android
+
+![demo](Demo/demo.gif)
